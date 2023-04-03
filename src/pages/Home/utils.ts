151fb3dcc7dc1450c3046b8da14bba68213a8c10
Krenedits Ferenc <fefe.kre@gmail.com>
2023-04-03 23:32:15 +0200

    deploy

--- src/pages/Home/utils.ts
+++
@@ -1,3 +0,0 @@
-const REGIONS = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-
-export { REGIONS };
--- src/utils/endpoints.ts
+++
@@ -1,6 +0,0 @@
-const endpoints = {
-    all: 'https://restcountries.com/v3.1/all',
-    byCode: 'https://restcountries.com/v3.1/alpha',
-};
-
-export default endpoints;
